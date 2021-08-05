86da291d8659c7fb4271b7bb1f37e12a69e52e5d
wachehi <wachehi@hotmail.com>
2021-08-05 16:17:23 +0200

    Modification du fichier app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'mon-projet en construction est en actualité';
+  title2 = 'mon-projet il va être construit très bien tôt';
+
+
 }
