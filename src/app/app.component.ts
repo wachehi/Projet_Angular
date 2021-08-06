2b00f0d39bf08ee64feec311c842ec311f04882d
wachehi <wachehi@hotmail.com>
2021-08-06 21:38:51 +0200

    dernier commit sur git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,8 @@ export class AppComponent {
   title2 = 'ajout branche dev verification';
   title3= 'ajout branche de ali';
   title4= 'ajout branche de ali pour une deuxième fois';
-  title5= 'ajout branche de ali pour une troisième fois';
+  title5= 'ajout branche de ali pour une troisiègitme fois';
+  title6= 'ajout branche de ali pour une troisiègitme fois';
 
 
 }
